be41c6eacb0e1110c07e1e15f13e34f905546972
v-iacovella <vittorio.iacovella@gmail.com>
2015-10-08 18:38:58 +0200

    Correct paths

--- data-analysis/func_data_preprocessing.sh
+++ data-analysis/func_data_preprocessing.sh
@@ -7,46 +7,32 @@ run=$4
 
 dummy_scans=$5
 
-tmp_yes_no=$6
-
-#my_curr_root=$(pwd)
-#curr_file=$(basename $1 .nii)
-#curr_dir=$(dirname $1)
-#target_dir=$2
-
-#mkdir -p $target_dir
-
-#curr_part=$(echo $curr_file | awk -F "." '{print $1}')
-#curr_task=$(echo $curr_file | awk -F "." '{print $2}')
-#curr_sess=$(echo $curr_file | awk -F "." '{print $3}')
-
+#tmp_yes_no=$6
 
 
 target_dir="./results/preproc/"$participant
 current_directory=$(pwd)
-mkdir $target_dir
+mkdir -pv $target_dir
 curr_file=$participant"_"$task"_"$run"_bold"
-echo $curr_file
 
-cd ./data/$participant/$session/func
+n_scans=$(fslinfo ./data/$participant/$session/func/$curr_file".nii.gz" | grep ^dim4 | awk '{print $2}')
+tot_scans=$(echo $n_scans-$dummy_scans | bc)
+echo $tot_scans
+echo
 
-	n_scans=$(fslinfo $curr_file | grep ^dim4 | awk '{print $2}')
+# Dummy Scans
+fslroi ./data/$participant/$session/func/$curr_file".nii.gz" $target_dir"/tmp.ds."$curr_file.nii.gz $5 $tot_scans
+echo Dummy Scans: DONE
 
-	tot_scans=$(echo $n_scans-$dummy_scans | bc)
-	echo $tot_scans
+# Slice Timing
+slicetimer -i $target_dir"/tmp.ds."$curr_file.nii.gz -o $target_dir"/tmp.ds.st."$curr_file.nii.gz -r 2 -d 2 --odd
+echo Slice Timing: DONE
 
-	# Dummy Scans
-	fslroi $curr_file.nii.gz $target_dir"/tmp.ds."$curr_file.nii.gz $5 $tot_scans
+# Brain Extraction
+bet $target_dir"/tmp.ds.st."$curr_file.nii.gz $target_dir"/tmp.ds.st.bet."$curr_file.nii.gz -F
+echo Brain Extraction: DONE
 
+# Motion Correction
+mcflirt -in $target_dir"/tmp.ds.st.bet."$curr_file.nii.gz -o $target_dir"/ds.st.bet.moco."$curr_file.nii.gz
+echo Dummy Scans: DONE
 
-	# Slice Timing
-	slicetimer -i $target_dir"/tmp.ds."$curr_file.nii.gz -o $target_dir"/tmp.ds.st."$curr_file.nii.gz -r 2 -d 2 --odd
-
-
-	# Brain Extraction
-	bet $target_dir"/tmp.ds.st."$curr_file.nii.gz $target_dir"/tmp.ds.st.bet."$curr_file.nii.gz -F
-
-	# Motion Correction
-	mcflirt -in $target_dir"/tmp.ds.st.bet."$curr_file.nii.gz -o $target_dir"/tmp.ds.st.bet.moco."$curr_file.nii.gz
-
-cd $current_directory
